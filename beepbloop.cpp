e9e1d7574f3dda8921249b41932777b123bf476a
Loyd, Scott <Scott.Loyd@gtri.gatech.edu>
2018-08-13 13:22:16 -0400

    support for simultaneous sounds

--- beepbloop.cpp
+++ beepbloop.cpp
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
@@ -29,10 +30,8 @@ global bool running;
 global SDL_Window* window;
 global SDL_Surface* surface;
 global SDL_Renderer* renderer;
-global double dt_for_frame;
-global double time = 0;
 global TTF_Font* font;
-global uint8_t* stream_data;
+global uint8_t* stream_data = NULL;
 global uint32_t stream_len;
 global char message[64];
 
@@ -90,16 +89,17 @@ struct Sound {
 };
 
 #define MAX_SOUNDS 256
-#define MAX_NOTES 16
+#define MAX_SAMPLES 16
 struct EngineData {
 	bool pause;
 	AudioFormat fmt;
+	float t;
 
 	Sound sounds[MAX_SOUNDS];
 	uint32_t num_sounds;
 
-	SampleData notes[MAX_NOTES];
-	uint32_t num_notes;
+	SampleData samples[MAX_SAMPLES];
+	uint32_t num_samples;
 };
 
 intern void apply_filter(Filter filter, float* samples, uint32_t num_samples)
@@ -225,31 +225,58 @@ intern bool init(EngineData* engine)
 }
 
 
-intern void generate_audio(void* userdata, uint8_t* stream, int len)
+intern void generate_audio(void* userdata, uint8_t* raw_stream, int len)
 {
-	EngineData* engine = (EngineData*)userdata;
-	float* sample = (float*)stream;
+	EngineData* engine = (EngineData*)userdata; 
+	float* stream = (float*)raw_stream;
 	int num_samples = len / (2 * sizeof(float));
-	for (int i = 0; i < num_samples; i++)
+	if (engine->pause)
 	{
-		if (engine->data.sample_index < engine->data.num_samples && !engine->pause)
+		for (int stream_index = 0; stream_index < num_samples; stream_index++)
 		{
-			float raw_sample = engine->data.samples[engine->data.sample_index++];
-			*sample++ = raw_sample;
-			*sample++ = raw_sample;
+			*stream++ = 0;
+			*stream++ = 0;
 		}
-		else
+		return;
+	}
+
+	// Determine how many simultaneous sounds are playing for balance
+	const int MAX_SIMULTANEOUS_SOUNDS = 6;
+	uint32_t num_sounds = 0;
+	Sound* current_sounds[MAX_SIMULTANEOUS_SOUNDS];
+	for (uint32_t sound_index = 0; sound_index < engine->num_sounds; sound_index++)
+	{
+		Sound* sound = engine->sounds + sound_index;
+		if (engine->t >= sound->start_t && (engine->t - sound->start_t) < sound->duration)
 		{
-			*sample++ = 0;
-			*sample++ = 0;
+			current_sounds[num_sounds++] = sound;
+			assert(num_sounds < MAX_SIMULTANEOUS_SOUNDS);
 		}
 	}
+
+	float ratio = 1.0f / (float)num_sounds;
+	for (uint32_t sound_index = 0; sound_index < num_sounds; sound_index++)
+	{
+		Sound* sound = current_sounds[sound_index];
+		SampleData* sample = engine->samples + sound->sample_index;
+		stream = (float*)raw_stream;
+
+		for (int i = 0; i < num_samples; i++)
+		{
+			if (sample->sample_index < sample->num_samples)
+			{
+				float raw_sample = ratio * sample->samples[sample->sample_index++];
+				*stream++ = raw_sample;
+				*stream++ = raw_sample;
+			}
+		}
+	}	
 
 	if (!stream_data)
 	{
 		stream_data = (uint8_t*)malloc(len);
 	}
-	memcpy(stream_data, stream, len);
+	memcpy(stream_data, raw_stream, len);
 	stream_len = len;
 
 	//snprintf(message, ArrayLen(message), "%f - %d", phase_values[engine->data.sample_index], engine->data.sample_index);
@@ -258,16 +285,21 @@ intern void generate_audio(void* userdata, uint8_t* stream, int len)
 global const int num_harmonics = 15;
 //global float* harmonic_samples[num_harmonics];
 
-intern void synthesize_note(Note* n, EngineData* engine)
+intern uint32_t synthesize_note(Note* n, EngineData* engine)
 {	
-	for (uint32_t i = 0; i < engine->data.num_samples; i++)
+	assert(engine->num_samples < MAX_SAMPLES);
+	uint32_t sample_index = engine->num_samples++;
+	SampleData* sample = engine->samples + sample_index;
+	sample->num_samples = (uint32_t)(n->duration * engine->fmt.sample_rate);
+
+	size_t bytes = sizeof(float)*sample->num_samples;
+	sample->samples = (float*)malloc(bytes);
+	for (uint32_t i = 0; i < sample->num_samples; i++)
 	{
-		engine->data.samples[i] = 0;
+		sample->samples[i] = 0;
 	}
 
-	size_t bytes = sizeof(float)*engine->data.num_samples;
-
-	float amps[num_harmonics] = {
+	const float amps[num_harmonics] = {
 		1, 2, 0.95f, 0.95f, 0.45f, 2, 0.95f, 0.95f, 0.95f, 0.45f, 2, 0.95f, 0.95f, 0.45f, 2
 	};
 
@@ -281,10 +313,10 @@ intern void synthesize_note(Note* n, EngineData* engine)
 		amplitude *= amps[harmonic];
 		float phase = 0;
 		float phase_inc = freq / engine->fmt.sample_rate;
-		for (uint32_t i = 0; i < engine->data.num_samples; i++)
+		for (uint32_t i = 0; i < sample->num_samples; i++)
 		{
 			float val = amplitude * sinf(2.0f * (float)M_PI * phase);
-			engine->data.samples[i] += val;
+			sample->samples[i] += val;
 			//harmonic_samples[harmonic][i] = val;
 			//harmonic_samples[harmonic][i] = engine->data.samples[i];
 			phase += phase_inc;			
@@ -300,23 +332,25 @@ intern void synthesize_note(Note* n, EngineData* engine)
 	tri_vol.triangle.min = 0.95f;
 	tri_vol.triangle.max = 1;
 	tri_vol.time_between_samples = 1.0f / engine->fmt.sample_rate;
-	apply_filter(tri_vol, engine->data.samples, engine->data.num_samples);
+	apply_filter(tri_vol, sample->samples, sample->num_samples);
 		
 	tri_vol.triangle.half_period = 0.6f;
 	tri_vol.triangle.min = 0.9f;
 	tri_vol.triangle.max = 1;
 	tri_vol.time_between_samples = 1.0f / engine->fmt.sample_rate;
-	apply_filter(tri_vol, engine->data.samples, engine->data.num_samples);
+	apply_filter(tri_vol, sample->samples, sample->num_samples);
 
 	tri_vol.triangle.half_period = 0.1f;
 	tri_vol.triangle.min = 0.85f;
 	tri_vol.triangle.max = 1.;
-	apply_filter(tri_vol, engine->data.samples, engine->data.num_samples);
+	apply_filter(tri_vol, sample->samples, sample->num_samples);
 
 	Filter log_val = { Filter_VolumeLogDamping, 1.0f / engine->fmt.sample_rate };
 	log_val.log.gamma = 0.6f;
 	log_val.log.t0 = -2;
-	apply_filter(log_val, engine->data.samples, engine->data.num_samples);
+	apply_filter(log_val, sample->samples, sample->num_samples);
+
+	return sample_index;
 }
 
 intern void handle_event(SDL_Event* e)
@@ -329,14 +363,16 @@ intern void render(EngineData* engine)
 	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 	SDL_RenderClear(renderer);
 
-
-	SampleData* data = &engine->data;
 	int h_base = SCREEN_HEIGHT / 3;
+	int h_base2 = SCREEN_HEIGHT / 2;
+#if 0
+	SampleData* data = &engine->data;
 	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 	int x = SCREEN_WIDTH * data->sample_index / data->num_samples;
 	int y1 = 0;
 	int y2 = h_base * 2;
 	SDL_RenderDrawLine(renderer, x, y1, x, y2);
+#endif
 
 #if 0
 	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
@@ -359,8 +395,7 @@ intern void render(EngineData* engine)
 	}
 #endif
 
-#if 1
-	int h_base2 = SCREEN_HEIGHT / 2;
+#if 0
 	SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
 	for (uint32_t i = 0; i < data->num_samples; i++)
 	{
@@ -371,10 +406,10 @@ intern void render(EngineData* engine)
 	}
 #endif
 
-#if 0
+#if 1
 
 	// Render the current buffer
-	SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
 	float* curr_sample = (float*)stream_data;
 	uint32_t num_samples = stream_len / sizeof(float);
 	for (uint32_t i = 0; i < num_samples; i++)
@@ -403,25 +438,53 @@ intern void render(EngineData* engine)
 	SDL_RenderPresent(renderer);
 }
 
+
+intern void reset(EngineData* engine)
+{
+	engine->t = 0;
+	for (uint32_t sample_index = 0; sample_index < engine->num_samples; sample_index++)
+	{
+		engine->samples[sample_index].sample_index = 0;
+	}
+}
+
+intern void add_sound(Note* note, EngineData* engine, float t)
+{
+	Sound* sound = engine->sounds + engine->num_sounds++;
+	sound->duration = note->duration;
+	sound->sample_index = synthesize_note(note, engine);
+	sound->start_t = t;
+}
+
 int main(int argc, char** argv)
 {
 	EngineData engine = { 0 };
 	engine.fmt.samples_in_buffer = 4096;
 	engine.fmt.sample_rate = 48000;
 
-	Note n = { 0 };
-	n.freq = notes_frequencies[2]; // low E
-	n.amplitude = 1;
-	n.duration = 10;
+	Note low_e = { 0 };
+	low_e.freq = notes_frequencies[2]; // low E
+	low_e.amplitude = 1;
+	low_e.duration = 10;
+	add_sound(&low_e, &engine, 0);
 
-	engine.data.num_samples = (int)(engine.fmt.sample_rate * n.duration);
-	engine.data.samples = (float*)malloc(sizeof(float) * engine.data.num_samples);
-	synthesize_note(&n, &engine);
-	
+	Note b = { 0 };
+	b.freq = notes_frequencies[9];
+	b.amplitude = 1;
+	b.duration = 10;
+	add_sound(&b, &engine, 0.05f);
+
+	Note g_sh = { 0 };
+	g_sh.freq = notes_frequencies[18];
+	g_sh.amplitude = 1;
+	g_sh.duration = 10;
+	add_sound(&g_sh, &engine, 0.1f);
+		
 	if (!init(&engine))
 	{
 		return 0;
 	}
+	
 
 	uint64_t counter_freq = SDL_GetPerformanceFrequency();
 	uint64_t start_counter = SDL_GetPerformanceCounter();
@@ -440,7 +503,7 @@ int main(int argc, char** argv)
 			}
 			if (event.type == SDL_KEYDOWN)
 			{
-				engine.data.sample_index = 0;
+				reset(&engine);
 				//engine.pause = true;
 			}
 		}
@@ -448,9 +511,11 @@ int main(int argc, char** argv)
 		uint64_t end_counter = SDL_GetPerformanceCounter();
 		uint64_t ticks = end_counter - start_counter;
 		start_counter = end_counter;
-		dt_for_frame = (double)ticks / (double)counter_freq;
-		time += dt_for_frame;
+		double dt_for_frame = (double)ticks / (double)counter_freq;
+		engine.t += (float)dt_for_frame;
 		
+		snprintf(message, ArrayLen(message), "%02.06f", engine.t);
+
 		render(&engine);
 		SDL_Delay(1);
 		
